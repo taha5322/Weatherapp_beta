22e63686fce6d19acd53238ab238a9b5bced43c8
Taha <tahasiddiqui31@gmail.com>
2020-03-28 10:40:14 -0400

    beta version, no data binding or location control

---
+++ app/src/main/java/com/example/stormy/CurrentWeather.java
@@ -0,0 +1,129 @@
+package com.example.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+// Created by Taha Siddiqui
+// 2020-03-27
+public class CurrentWeather {
+    private String locationLabel;
+    private String icon;
+    private long time;
+    private double temperature;
+    private double humidity;
+    private double precipChance;
+    private String summary;
+    private String timeZone;
+
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public int getIconId(){
+        int iconId=0;
+        switch(icon) {
+
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+        }
+        return iconId;
+    }
+
+    public String getFormatteeTime(){
+        //you need the pattern formqtted in that way to get the: Hour: minute: am/pm
+        SimpleDateFormat format = new SimpleDateFormat("h:mm:a");
+
+        format.setTimeZone(TimeZone.getTimeZone(timeZone));
+
+        Date dateTime = new Date(time*1000);
+        //format() returns a formatted time when you give UNIX time
+        //in miliseconds. The JSON gives us UNIX time in seconds
+        //I multiplied by thousand and made a date object which is what
+        //the format() needs as an object to return the time while formatted.
+        return format.format(dateTime);
+    }
+
+    public String getLocationLabel() {
+        return locationLabel;
+    }
+
+    public void setLocationLabel(String locationLabel) {
+        this.locationLabel = locationLabel;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return precipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        this.precipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+}
---
+++ app/src/main/java/com/example/stormy/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.stormy;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private CurrentWeather currentWeather;
+    private TextView darkSkyAttribution;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //used to make the link work
+        darkSkyAttribution = findViewById(R.id.darkSkyAttribution);
+        darkSkyAttribution.setMovementMethod(LinkMovementMethod.getInstance());
+
+        String link ="https://api.darksky.net/forecast/";
+        String apiKey = "c043e98b63e555588028b88476bd1ef1";
+        double latitude= 37.8267;
+        double longitude = -122.4233;
+
+        String URL = "https://api.darksky.net/forecast/c043e98b63e555588028b88476bd1ef1/37.8267,-122.4233";
+
+        if(isNetworkAvailible()) {
+
+            //client is needed to send requests to the dark sky server
+            OkHttpClient client = new OkHttpClient();
+
+            //request is sent to server
+            Request request = new Request.Builder()
+                    .url(URL)
+                    .build();
+
+            //call is what takes the request and sends it to server
+            Call call = client.newCall(request);
+
+            //Response response = call.execute(); don't use because it is synchronous
+
+            //this is an asychronous method.
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    try {
+
+                        String jsonData = response.body().string();
+
+                        //response.body().string() is the entire string message
+                        //from the response.
+
+                        if (response.isSuccessful()) {//checks to see if we have response
+                            currentWeather = getCurrentDetails(jsonData);
+                        } else {
+                            alertUserAboutProblem();
+                        }
+                    } catch (IOException e) {
+                        Log.d(TAG, "onResponse: IO exception caught", e);
+                    } catch (JSONException e){
+                        Log.e(TAG, "onResponse: Json exception caught", e);
+                    }
+
+
+                }
+            });
+
+            //NOTE: the Call class has an execute function which then
+            //retrieves the response
+            //if you only do Response response = call.execute();
+            //you will get an error because of no try catch
+
+        } else{
+            Toast.makeText(this, R.string.network_unavailible_message,Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG,"From JSON: " + timezone);
+
+        //object inside the json file which has data we care about
+        JSONObject currentlyObject = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+
+        currentWeather.setHumidity(currentlyObject.getDouble("humidity"));
+        currentWeather.setTime(currentlyObject.getLong("time"));
+        currentWeather.setIcon(currentlyObject.getString("icon"));
+        currentWeather.setLocationLabel("Alcatraz Island");
+        currentWeather.setPrecipChance(currentlyObject.getDouble("precipProbability"));
+        currentWeather.setSummary(currentlyObject.getString("summary"));
+        currentWeather.setTemperature(currentlyObject.getDouble("temperature"));
+        currentWeather.setTimeZone(timezone);
+
+        Log.d(TAG, currentWeather.getFormatteeTime());
+
+        return currentWeather;
+    }
+
+
+    private boolean isNetworkAvailible() {
+        //connectivity manager answers queries about the state of network connectivity
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //needs a permission in android manifest called ACCESS_NETWORK_STATE
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailible = false;
+
+        //checks to see if network is connected
+          if(networkInfo.isConnected() && networkInfo != null){
+              isAvailible = true;
+              Log.d(TAG, "we have connection");
+        } else{
+              isAvailible = false;
+              Log.d(TAG, "no connection");
+          }
+          return isAvailible;
+    }
+
+    private void alertUserAboutProblem() {
+        //makes an instance of the dialog class we made
+        AlertDialogFragment dialog = new AlertDialogFragment();
+
+        //shows the dialog on the activity. Fragment manager, u jus use getFragmentManager
+        dialog.show(getFragmentManager(), "Error dialog");
+    }
+}
